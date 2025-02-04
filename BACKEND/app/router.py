7b241a3cc3115ac0ffa4f520a4caa940e4361b74
AthulyaMS <athulyamarysaji1996@gmail.com>
2025-02-04 16:27:58 +0530

    permission to AI role for downloading project and removing versification path in usfm

--- BACKEND/app/router.py
+++ BACKEND/app/router.py
@@ -1613,11 +1613,11 @@ async def generate_usfm(
             status_code=400, detail="Project directory not found under input path"
         )
     # Locate versification.json
-    versification_path = next(project_input_path.rglob("versification.json"), None)
+    versification_path = "versification.json"
     if not versification_path:
-        logger.error("versification.json not found in the project folder.")
+        logger.error("versification.json not found .")
         raise HTTPException(
-            status_code=400, detail="versification.json not found in the project folder"
+            status_code=400, detail="versification.json not found "
         )
     # Read versification.json
     with open(versification_path, "r", encoding="utf-8") as versification_file:
@@ -1708,15 +1708,11 @@ async def download_processed_project_zip(
     project_id: int,
     db: Session = Depends(dependency.get_db),
     current_user: User = Depends(auth.get_current_user),
-):
-    project = (
-        db.query(Project)
-        .filter(
-            Project.owner_id == current_user.user_id, Project.project_id == project_id
-        )
-        .first()
-    )
- 
+): 
+    project = db.query(Project).filter(Project.project_id == project_id).first()
+
+    if current_user.role != "AI" and project.owner_id != current_user.user_id:
+        raise HTTPException(status_code=403, detail="Access denied. Only the project owner or users with the AI role can download the ZIP file.")
     if not project:
         raise HTTPException(status_code=404, detail="Project not found.")
     
